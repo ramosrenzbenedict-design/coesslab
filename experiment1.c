b8097b351ed2e36d8c126dcd1df1967f4f00e1ed
ramosrenzbenedict-design <ramos.renzbenedict@ue.edu.ph>
2025-12-12 00:51:00 +0000

    testing2

--- experiment1.c
+++ experiment1.c
@@ -1,27 +1,25 @@
-#define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 
 int main(int argc, char *arg[])
 {
 	int pid; pid = fork();
-		if (pid < 0)
-			{
-				printf("fork failed");
-				exit (1);
-			}
-		else if (pid == 0)
-			{
-				execlp("whoami", "is", NULL);
-				exit (0);
-			}
-		else
-			{
-				printf("\nProcess ID is: %d\n", getpid());
-				wait(NULL);
-				exit (0);
-			}
+	if (pid < 0)
+	{
+		printf("fork failed");
+		exit (1);
+	}
+	else if (pid == 0)
+	{
+		execlp("whoami", "is", NULL);
+		exit (0);
+	}
+	else
+	{
+		printf("\nProcess ID is: %d\n", getpid());
+		wait(NULL);
+		exit (0);
+	}
 }
